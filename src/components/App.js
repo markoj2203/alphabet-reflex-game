7c991f23f9e71b15f0512a1067b4d12dc5c75ba9
MARKO JOVICIC <markoj2203@gmail.com>
2021-03-01 17:45:23 +0100

    adding check boxes

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,57 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
-  return <div className="container">App</div>;
+  const [isChecked, setIsChecked] = useState("");
+
+  const switchDifficulty = (event) => {
+    setIsChecked(event.currentTarget.id);
+  };
+
+  return (
+    <div className="container">
+      <div className="main-content">
+        <div className="form-check">
+          <input
+            className="form-check-input"
+            type="radio"
+            name="flexRadioDefault"
+            id="easy"
+            checked={isChecked === "easy" || isChecked === ""}
+            onChange={switchDifficulty}
+          />
+          <label className="form-check-label" htmlFor="flexRadioDefault1">
+            Easy
+          </label>
+        </div>
+        <div className="form-check">
+          <input
+            className="form-check-input"
+            type="radio"
+            name="flexRadioDefault"
+            id="medium"
+            checked={isChecked === "medium"}
+            onChange={switchDifficulty}
+          />
+          <label className="form-check-label" htmlFor="flexRadioDefault2">
+            Medium
+          </label>
+        </div>
+        <div className="form-check">
+          <input
+            className="form-check-input"
+            type="radio"
+            name="flexRadioDefault"
+            id="hard"
+            checked={isChecked === "hard"}
+            onChange={switchDifficulty}
+          />
+          <label className="form-check-label" htmlFor="flexRadioDefault3">
+            Hard
+          </label>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
